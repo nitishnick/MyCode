837b83a94d9b10e9e012e8567ba51404feefa953
Nitish Rohilla <hi.nitish@hotmail.com>
2017-11-10 16:50:31 +0530

    wrapper class

--- src/main/java/com/sapient/test/SampleDAO.java
+++ src/main/java/com/sapient/test/SampleDAO.java
@@ -1,10 +1,14 @@
 package com.sapient.test;
 
+/**
+ * Wrapper class.
+ * 
+ * @author nrohil
+ *
+ */
 public class SampleDAO extends AbstractDatasource {
 
 	public SampleDAO(DBConfig config) {
 		super(config);
-
 	}
-
 }
