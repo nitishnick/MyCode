059caf69f2f49b9f05470d0925b536893f323822
Nitish Rohilla <hi.nitish@hotmail.com>
2017-11-10 16:27:44 +0530

    formatting
    desc

--- src/main/java/com/sapient/data/TradeDataProperties.java
+++ src/main/java/com/sapient/data/TradeDataProperties.java
@@ -3,36 +3,42 @@ package com.sapient.data;
 import java.util.List;
 
 public class TradeDataProperties {
-	
+
 	private List<TradeData> tradeDataList;
 	private int quantity;
 	private int avgLimitPrice;
 	private String orderInstructionDetails;
-		
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public int getAvgLimitPrice() {
 		return avgLimitPrice;
 	}
+
 	public void setAvgLimitPrice(int avgLimitPrice) {
 		this.avgLimitPrice = avgLimitPrice;
 	}
+
 	public String getOrderInstructionDetails() {
 		return orderInstructionDetails;
 	}
+
 	public void setOrderInstructionDetails(String orderInstructionDetails) {
 		this.orderInstructionDetails = orderInstructionDetails;
 	}
+
 	public List<TradeData> getTradeDataList() {
 		return tradeDataList;
 	}
+
 	public void setTradeDataList(List<TradeData> tradeDataList) {
 		this.tradeDataList = tradeDataList;
 	}
-	
-}
 
+}
